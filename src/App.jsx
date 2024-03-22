119ae98c7f6eadca742d12175ec062d6c8e67e1a
Alondra Mora <mora_ac@yahoo.com>
2024-03-21 21:04:06 -0700

    created a p tag to hold the values of minutes and seconds

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,53 @@ import Button from './Button'
 
 function App() {
 
-const [timerCount, setTimerCount] = useState(10);
+// const [timerCount, setTimerCount] = useState(10);
+const [minutes, setMinutes] = useState(15);
+const [seconds, setSeconds] = useState(0)
 
 
   function handleStart(){
     console.log('start timer')
-    startCountdown(10);
+    // startCountdown(10);
+      startCountdownMinsAndSecs(15 , 0)
   }
 
-  function startCountdown(seconds){
-    let counter = seconds;
+  function startCountdownMinsAndSecs(minutes, seconds){
+    let mins = minutes;
+    let secs = seconds;
 
     const interval = setInterval(() => {
-      console.log(counter);
-      counter--;
-      setTimerCount(counter);
-      if(counter < 1 ){
-        clearInterval(interval)
-        setTimerCount('Time is up!')
-        console.log('stop timer')
+      console.log(mins + ':' + secs)
+    },1000)
+  }
 
-        setTimeout(() => {
-          setTimerCount(10);
-        }, 3000)
-      }
-    },1000);
-  };
+  // function startCountdown(seconds){
+  //   let counter = seconds;
+
+  //   const interval = setInterval(() => {
+  //     console.log(counter);
+  //     counter--;
+  //     setTimerCount(counter);
+  //     if(counter < 1 ){
+  //       clearInterval(interval)
+  //       setTimerCount('Time is up!')
+  //       console.log('stop timer')
+
+  //       setTimeout(() => {
+  //         setTimerCount(10);
+  //       }, 3000)
+  //     }
+  //   },1000);
+  // };
+
+  /*
+  PSUEDOCODE
+  - Create a string that holds the minutes and seconds
+    {minutes}:{seconds} example: 15:00
+  - When we start the timer, start counting down the seconds until they reach zero
+    once they reach zero, reset them to '00' AND decrrement the minutes by 1
+    Once minutes == 0, then stop the timer and show the times up message
+  */
 
 
 return (
@@ -40,7 +61,8 @@ return (
       <button className="mode-buttons">Short Break</button>
       <button className="mode-buttons">Long Break</button>
     </div>
-    <p id="displayTime">{timerCount}</p>
+    {/* <p id="displayTime">{timerCount}</p> */}
+    <p id="minutes-and-seconds">{minutes}:{seconds}</p>
     <Button onClick={handleStart}>Start Timer</Button>
       
     </>
